d5c87003d6d86673046bd43c217a5cddf8b4e015
mmounaji <92118864+Mmounaji176@users.noreply.github.com>
2023-07-30 12:34:36 +0000

    cgi

--- Cgi/Cgi.cpp
+++ Cgi/Cgi.cpp
@@ -1,182 +1,144 @@
-#include "CGI.hpp"
-#define GCI_BUFFERSIZE 1024
+#include "cgi.hpp"
 
-/*--------- Constructors & Destructor --------*/
-
-CGI::CGI(request &request, serverConfig &config) : scriptName("./cgi_bin/php-cgi")
+std::map<std::string, std::string> Cgi::get_env(char *file, request &req)
 {
-	std::map<std::string, std::string> headers = request.getHeaders();
-	if (headers.find("Auth-Scheme") != headers.end() && headers["Auth-Scheme"] != "")
-		this->env["AUTH_TYPE"] = headers["Authorization"];
-	this->env["REDIRECT_STATUS"] = "200";
-	this->env["GATEWAY_INTERFACE"] = "CGI/1.1";
-	this->env["REQUEST_METHOD"] = request.getMethod();
-	this->env["CONTENT_LENGTH"] =  std::to_string(request.getBodyLength());
-	this->env["CONTENT_TYPE"] = headers["Content-Type"];
-	this->env["REMOTE_ADDR"] = to_string(config.gethostPort().host);
-	this->env["QUERY_STRING"] = request.getQuery();
-	this->env["REQUEST_URI"] = request.getPath() + request.getQuery();
-	this->env["REMOTE_IDENT"] = headers["Authorization"];
-	this->env["REMOTE_USER"] = headers["Authorization"];
-	if (headers.find("Host") != headers.end())
-		this->env["SERVER_NAME"] = headers["Host"];
-	else
-		this->env["SERVER_NAME"] = "serverName";
-	this->env["SERVER_PORT"] = to_string(config.gethostPort().port);
-	this->env["SERVER_PROTOCOL"] = "HTTP/1.1";
-	this->env["SERVER_SOFTWARE"] = "Weebserv/1.0";
-	// HTTP_*
-	this->env["HTTP_COOKIE"] = headers["Cookie"];
-	this->env["HTTP_ACCEPT"] = headers["Accept"];
-	this->env["HTTP_ACCEPT_CHARSET"] = headers["Accept-Charset"];
-	this->env["HTTP_ACCEPT_ENCODING"] = headers["Accept-Encoding"];
-	this->env["HTTP_ACCEPT_LANGUAGE"] = headers["Accept-Language"];
-	this->env["HTTP_CONNECTION"] = headers["Connection"];
+    std::map<std::string, std::string> env;
+
+    env["REQUEST_METHOD"] = req.get_method();
+    env["CONTENT_TYPE"] = req.get_header("Content-Type");
+    env["CONTENT_LENGTH"] = std::to_string(req.get_body().size());
+    env["QUERY_STRING"] = req.get_query();
+    env["REDIRECT_STATUS"] = "200";
+    env["SCRIPT_FILENAME"] = file;
+    return env;
 }
 
-CGI::CGI(CGI const &src)
+
+std::string Cgi::run_cgi(request &r, Respond &res, std::vector<server> server)
 {
-	if (this != &src)
-		this->env = src.env;
+    // Extract the CGI file path and path info from the Respond object
+    char *file, *path;
+    file = strdup(res.get_file_cgi().c_str());
+    path = strdup(res.get_path_info_founded().c_str());
+
+    // Prepare the command to execute the CGI script
+    char *cmd[3] = {path, file, NULL};
+
+    // Create temporary file descriptors to capture CGI script output
+    std::string cgi_str;
+    FILE *temp = std::tmpfile();
+    FILE *temp1 = std::tmpfile();
+    int fdtemp = fileno(temp);
+    int fdtemp1 = fileno(temp1);
+
+    // Get the environment variables for the CGI script
+    std::map<std::string, std::string> env = get_env(file, r);
+    std::string method = env["REQUEST_METHOD"];
+
+    // Prepare the environment variables as an array of C strings
+    char **envp = (char **)malloc(sizeof(char *) * (env.size() + 1));
+    if (!envp)
+    {
+        res.handle_error_response(server, 500);
+        free(file);
+        free(path);
+        return res.get_response_status(res.get_status_code());
+    }
+    int i = 0;
+    for (std::map<std::string, std::string>::iterator it = env.begin(); it != env.end(); it++)
+    {
+        std::string e = it->first + "=" + it->second;
+        envp[i++] = strdup(e.c_str());
+    }
+    envp[i] = NULL;
+
+    // Fork a child process to execute the CGI script
+    int pid = fork();
+    if (pid == -1)
+    {
+        res.handle_error_response(server, 500);
+        free_all(file, path, envp, env.size());
+        return "";
+    }
+    else if (pid == 0)
+    {
+        // Child process (execute the CGI script)
+
+        // If the request method is POST, redirect the standard input to read from the temp1 file descriptor
+        if (method == "POST")
+        {
+            if (dup2(fdtemp1, STDIN_FILENO) == -1)
+            {
+                res.handle_error_response(server, 500);
+                free_all(file, path, envp, env.size());
+                return "";
+            }
+            write(fdtemp1, r.get_body().c_str(), r.get_body().size());
+            std::rewind(temp1);
+        }
+
+        // Redirect the standard output to write to the temp file descriptor
+        if (dup2(fdtemp, STDOUT_FILENO) == -1)
+        {
+            res.handle_error_response(server, 500);
+            free_all(file, path, envp, env.size());
+            return "";
+        }
+
+        // Set an alarm to limit the execution time of the CGI script (2 seconds in this case)
+        alarm(2);
+
+        // Execute the CGI script using execve
+        execve(cmd[0], cmd, envp);
+
+        // If the execution reaches this point, there was an error executing the CGI script
+        exit(1);
+    }
+
+    // Parent process (web server)
+
+    // Wait for the child process to complete
+    int status;
+    waitpid(pid, &status, 0);
+
+    // Check if the child process was terminated by a signal or if it exited with a non-zero status (indicating an error)
+    if (WIFSIGNALED(status) || status != 0)
+    {
+        res.handle_error_response(server, 500);
+        free_all(file, path, envp, env.size());
+        return "";
+    }
+
+    // Read the output generated by the CGI script from the temp file descriptor and store it in the 'content' string
+    char buf[1];
+    int byt;
+    std::string content;
+    std::rewind(temp);
+    while ((byt = read(fdtemp, buf, 1)) > 0)
+        content.append(buf, 1);
+
+    // Check for errors while reading the output
+    if (byt == -1)
+    {
+        res.set_status_code(500);
+        res.set_response_body(res.get_response_status(res.get_status_code()));
+        free_all(file, path, envp, env.size());
+        return res.get_response_status(res.get_status_code());
+    }
+
+    // Close the temporary file descriptors and free allocated memory
+    close(fdtemp);
+    close(fdtemp1);
+    free_all(file, path, envp, env.size());
+
+    // Process the CGI script output to set response headers and body
+    set_headers_cgi(content, res);
+
+    // Return the CGI script output as a string
+    return content;
 }
 
-CGI::~CGI(void){
-
-}
-
-CGI &CGI::operator=(CGI const &src){
-	if (this != &src)
-		this->env = src.env;
-	return *this;
-}
-
-/*--------- Member functions --------*/
-
-char **mapToArray(std::map<std::string, std::string> env)
-{
-	char **env;
-	int j = 0;
-
-	try{
-		env = new char *[env.size() + 1];
-		for (std::map<std::string, std::string>::const_iterator i = env.begin();
-			 i != env.end(); i++)
-		{
-			std::string line = i->first + "=" + i->second;
-			env[j] = new char[line.size() + 1];
-			env[j] = strcpy(env[j], line.c_str());
-			j++;
-		}
-		env[j] = NULL;
-	}
-	catch (std::bad_alloc &e)
-	{
-		std::cerr << RED << e.what() << RESET << std::endl;
-	}
-	return env;
-}
-
-void deleteArray(char **env)
-{
-	for (size_t i = 0; env[i]; i++)
-		delete[] env[i];
-	delete[] env;
-}
-
-/*
-	main function for executing cgi
-*/
-
-std::string CGI::executeCgi(const std::string &_filePath, size_t socket_fd, Response &response, request &request)
-{
-	std::fstream fileOut;	
-	std::string output;
-	pid_t pid;
-	int savedIn;
-	int savedOut;
-	char **env;
-	std::string filename = "./tmp/body";
-	filename += to_string(socket_fd);
-	std::fstream _body;
-	std::string myline;
-	char **argv = new char *[3];
-	argv[0] = new char[scriptName.size() + 1];
-	argv[0] = strcpy(argv[0], scriptName.c_str());
-	argv[1] = new char[_filePath.size() + 1];
-	argv[1] = strcpy(argv[1], _filePath.c_str());
-	argv[2] = NULL;
-
-	this->env["PATH_INFO"] = _filePath;
-	this->env["PATH_TRANSLATED"] = _filePath;
-	env = mapToArray(this->env);
-
-	savedIn = dup(STDIN_FILENO);
-	savedOut = dup(STDOUT_FILENO);
-
-	response.get_file_change_get() = randomFileName();
-	int fdIn = open(filename.c_str(), O_RDWR | O_CREAT, 0666);
-	int fdOut = open(response.get_file_change_get().c_str(),  O_RDWR | O_CREAT, 0666);
-
-	lseek(fdIn, 0, SEEK_SET);
-	pid = fork();
-	if (pid == -1){
-		std::cerr << "Fork Error" << std::endl;
-		response.set_status_code(500);
-	}
-	else if (pid == 0){
-		dup2(fdIn, STDIN_FILENO);
-		dup2(fdOut, STDOUT_FILENO);
-		execve(scriptName.c_str(), argv, env);
-		response.set_status_code(500);
-	}
-	else
-	{
-		char buffer[GCI_BUFFERSIZE] = {0};
-		int ret = 1;
-
-		waitpid(pid, NULL, 0);
-		lseek(fdOut, 0, SEEK_SET);
-		while (ret > 0)
-		{
-			memset(buffer, 0, GCI_BUFFERSIZE);
-			ret = read(fdOut, buffer, GCI_BUFFERSIZE - 1);
-			output += buffer;
-		}
-		dup2(savedIn, STDIN_FILENO);
-		dup2(savedOut, STDOUT_FILENO);
-	}
-	fileOut.close();
-	close(fdIn);
-	close(fdOut);
-	close(savedIn);
-	close(savedOut);
-	deleteArray(env);
-	return addHeader(socket_fd, output, response, request);
-}
-
-
-std::string CGI::addHeader(size_t socket_fd, const std::string &responseData, Response &response, request &request) {
-    // Compose the HTTP response message, including the response headers
-    std::string httpResponse;
-
-    // Add the status line (HTTP version and status code)
-    httpResponse += "HTTP/1.1 " + std::to_string(response.get_status_code()) + " " + response.get_status_message() + "\r\n";
-
-    // Add other necessary headers (e.g., Content-Type, Content-Length, etc.) to the response
-    httpResponse += "Content-Type: " + response.get_content_type() + "\r\n";
-    httpResponse += "Content-Length: " + std::to_string(responseData.size()) + "\r\n";
-
-    // Add any additional custom headers as needed based on the response and request objects
-
-    // Add a blank line to separate the headers from the response body
-    httpResponse += "\r\n";
-
-    // Add the response data to the HTTP response message
-    httpResponse += responseData;
-
-    // Send the composed HTTP response back to the client using the socket_fd
-    send(socket_fd, httpResponse.c_str(), httpResponse.size(), 0);
-
-    // Return the complete HTTP response for logging or other purposes (not necessary for actual response sending)
-    return httpResponse;
+bool isCGIRequest(const std::string& url) {
+    // Check if the URL ends with the ".cgi" extension
+    return url.length() >= 4 && url.substr(url.length() - 4) == ".cgi";
 }
--- Cgi/Cgi.hpp
+++ Cgi/Cgi.hpp
@@ -1,32 +1,20 @@
-
-#ifndef __CGI_HPP__
-#define __CGI_HPP__
+#ifndef CGI_H
+#define CGI_H
 
 #include <iostream>
-#include <string>
-#include <map>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <vector>
+#include <map>
+#include <string>
+#include "../request/request.hpp"
+#include "../respond/respond.hpp"
 
-class Response;
+class Respond;
 class request;
 
-class CGI
-{
-	private:
-		std::map<std::string, std::string>	env;
-		std::string							filePath;
-		std::string							scriptName;
-	public:
-		CGI(CGI const &);
-		~CGI(void);
-		CGI	&operator=(CGI const &src);
-		CGI( request &request,  serverConfig &server);
-		std::string		executeCgi(const std::string&, size_t, Response&, request &);
-		std::string		getTheCompletePath(const std::string&);
-		std::string		addHeader(int fd, std::string, Response &, request &);
-};
+std::string run_cgi(request &r,  Respond &res, std::vector<server> server);
+std::map<std::string, std::string> get_env(char *file, request req);
+
 
 #endif
